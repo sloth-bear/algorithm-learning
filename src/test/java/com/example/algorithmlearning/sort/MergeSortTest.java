48d6d310e315c14c184dc93c83a645c23690169d
sloth-bear <slothbear.hj@gmail.com>
2022-12-01 23:35:32 +0900

    Add MergeSortTest test

--- src/test/java/com/example/algorithmlearning/sort/MergeSortTest.java
+++ src/test/java/com/example/algorithmlearning/sort/MergeSortTest.java
@@ -2,6 +2,8 @@ package com.example.algorithmlearning.sort;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.sql.Array;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
@@ -11,15 +13,22 @@ class MergeSortTest {
   @Test
   void thenAscSorted_whenMergeSort_givenRandomIntegers() {
     // given
-    final var list = new ArrayList<Integer>();
-    SortTestUtils.addHundredRandoms(list);
+    final var list1 = new ArrayList<Integer>();
+    SortTestUtils.addHundredRandoms(list1);
 
-    final var expected = new ArrayList<>(list);
-    Collections.sort(expected);
+    final var list2 = new ArrayList<>(Arrays.asList(10,9,8,7,6,5));
+
+    final var expected1 = new ArrayList<>(list1);
+    Collections.sort(expected1);
+    final var expected2 = new ArrayList<>(list2);
+    Collections.sort(expected2);
 
     // when
-    final var result = MergeSort.sort(list);
-    assertThat(result).isEqualTo(expected);
+    final var result1 = MergeSort.sort(list1);
+    assertThat(result1).isEqualTo(expected1);
+
+    final var result2 = MergeSort.sort(list2);
+    assertThat(result2).isEqualTo(expected2);
   }
 
 }
