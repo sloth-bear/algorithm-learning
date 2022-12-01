1041697abbe9f7d54d1c8615bb9ba32899af2010
sloth-bear <slothbear.hj@gmail.com>
2022-12-01 21:11:08 +0900

    Add FibonacciAlgorithm by using dynamic programming

--- src/main/java/com/example/algorithmlearning/bruteforce/FibonacciAlgorithm.java
+++ src/main/java/com/example/algorithmlearning/bruteforce/FibonacciAlgorithm.java
@@ -6,7 +6,25 @@ public class FibonacciAlgorithm {
     throw new UnsupportedOperationException();
   }
 
-  public static int get(final int n) {
-    return n <= 1 ? n : get(n-1) + get(n-2);
+  public static int recursive(final int n) {
+    return n <= 1 ? n : recursive(n-1) + recursive(n-2);
   }
+
+  public static int dp(final int n) {
+    final int[] cache = new int[n+1];
+    cache[0] = 0;
+
+    if (n==0) {
+      return cache[n];
+    }
+
+    cache[1] = 1;
+
+    for (int i = 2; i < n+1; i++) {
+      cache[i] = cache[i-1] + cache[i-2];
+    }
+
+    return cache[n];
+  }
+
 }
--- src/test/java/com/example/algorithmlearning/bruteforce/FibonacciAlgorithmTest.java
+++ src/test/java/com/example/algorithmlearning/bruteforce/FibonacciAlgorithmTest.java
@@ -1,16 +1,14 @@
 package com.example.algorithmlearning.bruteforce;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class FibonacciAlgorithmTest {
 
   @Test
-  void thenReturnFibonacci_whenGet_givenNaturalInteger() {
+  void thenReturnFibonacci_whenRecursive_givenNaturalInteger() {
     // given
     final var number1 = (int) (Math.random() * 10);
     final var number2 = (int) (Math.random() * 10);
@@ -18,8 +16,24 @@ class FibonacciAlgorithmTest {
     final var fibonacciSequences = List.of(0, 1, 1, 2, 3, 5, 8, 13, 21, 34);
     
     // when
-    final var result1 = FibonacciAlgorithm.get(number1);
-    final var result2 = FibonacciAlgorithm.get(number2);
+    final var result1 = FibonacciAlgorithm.recursive(number1);
+    final var result2 = FibonacciAlgorithm.recursive(number2);
+
+    // then
+    assertThat(result1).isEqualTo(fibonacciSequences.get(number1));
+    assertThat(result2).isEqualTo(fibonacciSequences.get(number2));
+  }
+
+  @Test
+  void thenReturnFibonacci_whenDp_givenNaturalInteger() {
+    final var number1 = (int) (Math.random() * 10);
+    final var number2 = (int) (Math.random() * 10);
+
+    final var fibonacciSequences = List.of(0, 1, 1, 2, 3, 5, 8, 13, 21, 34);
+
+    // when
+    final var result1 = FibonacciAlgorithm.dp(number1);
+    final var result2 = FibonacciAlgorithm.dp(number2);
 
     // then
     assertThat(result1).isEqualTo(fibonacciSequences.get(number1));
